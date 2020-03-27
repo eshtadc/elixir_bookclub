7d93aa449d9a24420c60cc62930e698ba1012e2e
Lisa Backer <lisa.haas.backer@gmail.com>
2020-03-27 13:59:42 -0400

    fixes

--- dungeon_crawl/lib/dungeon_crawl/rest.ex
+++ dungeon_crawl/lib/dungeon_crawl/rest.ex
@@ -4,7 +4,7 @@ defmodule DungeonCrawl.Rest do
 
   # can rest up to three times
   # rest_and_heal(character, count)
-  def rest(character), do: rest_and_heal(character, 3)
+  def heal(character), do: rest_and_heal(character, 3)
 
   defp rest_and_heal(character, 0) do
     # This is your last rest so you must go back
@@ -13,8 +13,8 @@ defmodule DungeonCrawl.Rest do
     Shell.cmd("clear")
     character
   end
-  defp rest_and_heal(%{hit_points, max_hit_points}, _)
-    when current_hit_points = max_hit_points do
+  defp rest_and_heal(%{hit_points: current_hit_points, max_hit_points: max_hit_points} = character, _)
+    when current_hit_points == max_hit_points do
       #You are all healed
       Shell.info("You have overstayed your welcome. It's time to continue")
       Shell.prompt("Press Enter to continue")
@@ -24,21 +24,21 @@ defmodule DungeonCrawl.Rest do
 
   defp rest_and_heal(character, remaining) do
     # You heal a random amount
-    healed_character = heal(character, 1) # maybe make this random?
+    healed_character = Character.heal(character, 1) # maybe make this random?
 
     healed_character
     |> heal_message
     |> Shell.info
 
     # Do you want to stay or return
-    if Shell.yes?(confirm_stay), do: rest_and_heal(healed_character, remaining - 1), else: healed_character
+    if Shell.yes?(confirm_stay()), do: rest_and_heal(healed_character, remaining - 1), else: healed_character
   end
 
   defp heal_message(character) do
-    "Rest has been kind to you.\n#{current_stats(character)}"
+    "Rest has been kind to you.\n#{Character.current_stats(character)}"
   end
 
-  defp confirm_stay(character) do
+  defp confirm_stay() do
     "Do you want to rest more?"
   end
 end
--- dungeon_crawl/lib/dungeon_crawl/room/triggers/heal.ex
+++ dungeon_crawl/lib/dungeon_crawl/room/triggers/heal.ex
@@ -1,12 +1,12 @@
-defmodule DungeonCrawl.Room.Triggers.Enemy do
+defmodule DungeonCrawl.Room.Triggers.Heal do
   @behaviour DungeonCrawl.Room.Trigger
 
   alias Mix.Shell.IO, as: Shell
 
   def run(character, %DungeonCrawl.Room.Action{id: :rest}) do
     Shell.info("You settle down for a little rest.")
-    healed_character = DungeonCrawl.Rest(character)
+    healed_character = DungeonCrawl.Rest.heal(character)
 
-    {updated_char, :forward}
+    {healed_character, :forward}
   end
 end
